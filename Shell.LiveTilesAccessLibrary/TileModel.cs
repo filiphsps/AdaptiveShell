c92a1fb36dc156473ba8d6bf716e3642b63159e9
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-12-03 18:05:59 +0100

    chore(repo): Initial commit.

---
+++ Shell.LiveTilesAccessLibrary/TileModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shell.LiveTilesAccessLibrary {
+    public class TileModel {
+        public String AppId;
+        public String Payload;
+    }
+}
---
+++ Shell/SplashPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Windows.Foundation.Metadata;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Shell {
+    public sealed partial class SplashPage : Page {
+        public SplashPage() {
+            this.InitializeComponent();
+        }
+
+        private void SplashPage_OnLoaded(Object sender, RoutedEventArgs e) {
+            // TODO
+            this.Frame.Navigate(typeof(Pages.StartPage));
+            this.Frame.BackStack.Remove(this.Frame.BackStack.Last());
+        }
+    }
+}
