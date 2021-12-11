e3494ddcf7ac4cc0c31dabec21194a876d07de10
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-12-11 04:30:17 +0100

    chore(host): Start work on wpf shellhost.

---
+++ Shell.Host/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shell.Host {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            this.InitializeComponent();
+
+            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Right - this.Width;
+            this.Top = desktopWorkingArea.Bottom;
+        }
+
+        private void Window_Deactivated(Object sender, EventArgs e) {
+            var window = (Window)sender;
+            window.Topmost = true;
+        }
+
+        private void StartBtn_Click(Object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+    }
+}
