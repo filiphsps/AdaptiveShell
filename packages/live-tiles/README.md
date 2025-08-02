975a021150d65f109f770385da0148c1bbc1a886
Filiph Siitam Sandström <filfat@hotmail.se>
2025-08-02 13:41:37 +0200

    docs: Add `jsdoc` to two live-tile classes

--- packages/live-tiles/README.md
+++ packages/live-tiles/README.md
@@ -1,3 +1,4 @@
 # @metro-ui/live-tiles
 
-[![npm](https://img.shields.io/npm/dt/@metro-ui/live-tiles)](https://www.npmjs.com/package/@metro-ui/live-tiles)
+[![npm downloads](https://img.shields.io/npm/dt/@metro-ui/live-tiles.svg)](https://www.npmjs.com/package/@metro-ui/live-tiles)
+[![npm version](https://img.shields.io/npm/v/@metro-ui/live-tiles.svg?style=flat)](https://www.npmjs.com/package/@metro-ui/live-tiles)
--- packages/live-tiles/src/tiles/live-tiles.ts
+++ packages/live-tiles/src/tiles/live-tiles.ts
@@ -76,6 +76,7 @@ export abstract class LiveTileImage {
         public readonly overlay: number = 20
     ) {}
 }
+export class LiveTileBadgeImage extends LiveTileImage {}
 export class LiveTilePeekImage extends LiveTileImage {}
 export class LiveTileBackgroundImage extends LiveTileImage {}
 
@@ -86,6 +87,42 @@ export class LiveTileContentAdaptive extends LiveTileContent {
         public readonly backgroundImage: Optional<LiveTileBackgroundImage>,
         public readonly textStacking: AdaptiveTextStacking = AdaptiveTextStacking.Default,
         public readonly children: AdaptiveContent[] = []
+    ) {
+        super();
+    }
+}
+
+/**
+ * A tile that displays a single line of text with an optional badge.
+ *
+ * ![screenshot](https://learn.microsoft.com/en-us/previous-versions/windows/apps/images/hh761491.tilesquare150x150iconwithbadge(en-us,win.10).jpg)
+ *
+ *{@link https://learn.microsoft.com/en-us/previous-versions/windows/apps/hh761491(v=win.10)#tilesquare150x150iconwithbadge}
+ * @extends {LiveTileContent}
+ */
+export class LiveTileContentIconWithBadge extends LiveTileContent {
+    public constructor(
+        public readonly badgeImage: Optional<LiveTileBadgeImage>,
+        public readonly text: string = ''
+    ) {
+        super();
+    }
+}
+
+/**
+ * A tile that displays two rows of text either side by side or one above the other depending on the device/platform.
+ * They were typically used for display the day and month the calendar app tile.
+ *
+ * ![screenshot](https://learn.microsoft.com/en-us/previous-versions/windows/apps/images/hh761491.tilesquareblock(en-us,win.10).png)
+ *
+ * {@link https://learn.microsoft.com/en-us/previous-versions/windows/apps/hh761491(v=win.10)#tilesquareblocktilesquare150x150block}
+ * @extends {LiveTileContent}
+ */
+export class LiveTileContentTileSquareBlock extends LiveTileContent {
+    public constructor(
+        public readonly blockText: Optional<string>,
+        public readonly besideBlockText: Optional<string>,
+        public readonly text: string = ''
     ) {
         super();
     }
